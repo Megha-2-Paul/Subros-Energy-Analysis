76fb0bc166ad0e40806ccf95c5bfe5a7f8e8937c
Megha-2-Paul <meghapaul0202@gmail.com>
2025-06-24 10:43:44 +0530

    Removed SHAP

--- advanced_modules.py
+++ advanced_modules.py
@@ -14,19 +14,3 @@ def run_kmeans_visualization(df, numeric_cols):
         st.plotly_chart(fig)
     except Exception as e:
         st.warning(f"KMeans failed: {e}")
-
-# def run_shap_explainer(df):
-#     st.subheader("🧠 SHAP Explainability")
-#     try:
-#         if all(col in df.columns for col in ['DG_UNIT', 'UPPCL_(UNIT_)', 'TOTAL_UNIT_(UPPCL+DG)']):
-#             X = df[['DG_UNIT', 'UPPCL_(UNIT_)']]
-#             y = df['TOTAL_UNIT_(UPPCL+DG)']
-#             model = RandomForestRegressor().fit(X, y)
-#             explainer = shap.Explainer(model, X)
-#             shap_values = explainer(X)
-#             st.set_option('deprecation.showPyplotGlobalUse', False)
-#             st.pyplot(shap.plots.beeswarm(shap_values, show=False))
-#         else:
-#             st.warning("Missing required columns for SHAP.")
-#     except Exception as e:
-#         st.warning(f"SHAP failed: {e}")
